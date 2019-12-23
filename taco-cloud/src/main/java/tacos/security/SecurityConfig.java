75761cdcfa4adad0fd3e3b5c455b4ace9d29dbb1
maor.zaken <maorzaken@gmail.com>
2019-12-23 10:02:08 +0200

    taco-cloud v4.2.4 listing 4.7

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -1,25 +1,29 @@
 package tacos.security;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
 	@Autowired
-	private DataSource dataSource;
+	private UserDetailsService userDetailsService;
 
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(dataSource)
-			.usersByUsernameQuery("select username, password, enabled from Users where username=?")
-				.authoritiesByUsernameQuery("select username, authority from UserAuthorities where username=?")
-				.passwordEncoder(new StandardPasswordEncoder("53cr3t"));
+		auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
+	}
+	
+	@Bean
+	public PasswordEncoder encoder() {
+	  return new StandardPasswordEncoder("53cr3t");
 	}
 }
