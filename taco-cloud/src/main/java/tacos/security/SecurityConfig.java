a50bf5416738169dc9c3179a41dc7f6cb1c66c9c
maor.zaken <maorzaken@gmail.com>
2019-12-24 10:04:52 +0200

    4.3.2 middle

--- taco-cloud/src/main/java/tacos/security/SecurityConfig.java
+++ taco-cloud/src/main/java/tacos/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -26,4 +27,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public PasswordEncoder encoder() {
 	  return new StandardPasswordEncoder("53cr3t");
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		  .authorizeRequests().antMatchers("/design", "/orders")
+		  	.hasRole("ROLE_USER")
+		  .antMatchers("/", "/**").permitAll()
+		  
+		  .and().formLogin().loginPage("/login").defaultSuccessUrl("/design");
+	}
+	
+
 }
